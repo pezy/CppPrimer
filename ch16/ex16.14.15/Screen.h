ea99cabbeddfc7bb2216b8c663de6d9f5ab7f655
magichan <magiczhangyukun@gmail.com>
2015-05-05 15:45:58 +0800

    merge #236

--- ch16/ex16.14.15/Screen.h
+++ ch16/ex16.14.15/Screen.h
@@ -22,6 +22,25 @@ public:
     Screen& operator<< (const char &c);
     Screen& operator>> (char &c);
 
+    friend std::ostream & operator<< ( std::ostream &os , const Screen<H,W> & c )
+    {
+        unsigned int i,j;
+        for( i=0 ;i<c.height; i++ )
+        {
+                os<<c.contents.substr(0,W)<<std::endl;
+        }
+        return os;
+    }
+
+    friend std::istream & operator>> ( std::istream &is , Screen &  c )
+    {
+            char a;
+            is>>a;
+            std::string temp(H*W,a);
+            c.contents = temp;
+            return is;
+    }
+
 private:
     pos cursor = 0;
     pos height = H, width = W;
