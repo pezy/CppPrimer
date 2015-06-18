8d302840fbfe5d1e24c65799ef7d6286cad7b79a
pezy <urbancpz@gmail.com>
2015-06-18 16:49:27 +0800

    fixed #33

--- ch09/ex9_43.cpp
+++ ch09/ex9_43.cpp
@@ -2,7 +2,7 @@
 //  ex9_43.cpp
 //  Exercise 9.43
 //
-//  Created by pezy on 12/4/14.
+//  Created by pezy on 6/18/15.
 //  Copyright (c) 2014 pezy. All rights reserved.
 //
 //  @Brief  Write a function that takes three strings, s, oldVal, and newVal.
@@ -10,30 +10,48 @@
 //          all instances of oldVal that appear in s by newVal.
 //          Test your function by using it to replace common abbreviations,
 //          such as "tho" by "though" and "thru" by "through".
-//  @Compile    Gcc can't support return iterator of string::insert. Please use clang or VS2013
+
+#include <string>
+using std::string;
 
 #include <iostream>
-#include <string>
 
-using std::cout; using std::endl; using std::string; using std::prev;
-
-void Replace(string &s, string const& oldVal, string const& newVal) {
-    auto val_size = oldVal.size();
-    if (s.size() < val_size) return;
-
-    for (auto iter = s.begin(), end = prev(s.end(), val_size - 1); iter != end; ++iter) {
-        if (string{iter, iter + val_size} == oldVal) {
-            iter = s.earse(iter, iter + val_size);
-            iter = s.insert(iter, newVal.cbegin(), newVal.cend()); // gcc 4.9 bug (see http://stackoverflow.com/questions/29690369)
-            iter += newVal.size();
+void Replace(string &s, const string &oldVal, const string &newVal) {
+    for (auto beg = s.begin(); beg != s.end(); ++beg) {
+        if (*beg != oldVal.front()) continue;
+        if (std::distance(beg, s.end()) < std::distance(oldVal.begin(), oldVal.end())) break;
+        if (string{beg, beg + oldVal.size()} == oldVal) {
+            auto pos = std::distance(s.begin(), beg);
+            s.erase(beg, beg + oldVal.size());
+            s.insert(beg, newVal.cbegin(), newVal.cend());
+            beg = std::next(s.begin(), pos + newVal.size() - 1);
         }
     }
 }
 
-int main()
-{
-    string str{"To drive straight thru is a foolish, tho courageous act."};
-    Replace(str, "tho", "though");
-    Replace(str, "thru", "through");
-    cout << str << endl;
+int main() {
+    {
+        string str{"To drive straight thru is a foolish, tho courageous act."};
+        Replace(str, "thru", "through");
+        Replace(str, "tho", "though");
+        std::cout << str << std::endl;
+    }
+    {
+        string str{"To drive straight thruthru is a foolish, thotho courageous act."};
+        Replace(str, "thru", "through");
+        Replace(str, "tho", "though");
+        std::cout << str << std::endl;
+    }
+    {
+        string str{"To drive straight thru is a foolish, tho courageous act."};
+        Replace(str, "thru", "thruthru");
+        Replace(str, "tho", "though");
+        std::cout << str << std::endl;
+    }
+    {
+        string str{"my world is a big world"};
+        Replace(str, "world", "worldddddddddddddddddddddddddddddddddddddddddddddddd");
+        std::cout << str << std::endl;
+    }
+    return 0;
 }
