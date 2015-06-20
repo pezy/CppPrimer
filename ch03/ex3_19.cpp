2ee9cf18fb4a99220ff144d78bfb324b35418cdd
Yue Wang <Moophyalan@gmail.com>
2015-06-21 03:03:55 +1200

    improving ex3_19.cpp

--- ch03/ex3_19.cpp
+++ ch03/ex3_19.cpp
@@ -12,13 +12,10 @@ using std::vector;
 
 int main()
 {
-  vector<int> ivec1(10, 42);
-  vector<int> ivec2{42, 42, 42, 42, 42, 42, 42, 42, 42, 42};
-  vector<int> ivec3;
-  for (int i=0; i != 10; ++i)
-    ivec3.push_back(42);
-
-  std::cout << "The first one more better!" << std::endl;
-
-  return 0;
+    vector<int> ivec1(10, 42);
+    vector<int> ivec2{42, 42, 42, 42, 42, 42, 42, 42, 42, 42};
+    vector<int> ivec3;
+    for (int i=0; i != 10; ++i)
+        ivec3.push_back(42);
+    std::cout << "The first approach is better!" << std::endl;
 }
