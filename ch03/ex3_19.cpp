c493c6ed4b25982d23ec31c379ebcf86a9ec44a6
pezy <urbancpz@gmail.com>
2015-06-22 00:01:05 +0800

    fixed ambiguous of ex3_19

--- ch03/ex3_19.cpp
+++ ch03/ex3_19.cpp
@@ -15,10 +15,10 @@ int main()
   vector<int> ivec1(10, 42);
   vector<int> ivec2{42, 42, 42, 42, 42, 42, 42, 42, 42, 42};
   vector<int> ivec3;
-  for (decltype(ivec3.size()) i=0; i != 10; ++i)
+  for (int i=0; i != 10; ++i)
     ivec3.push_back(42);
 
-  std::cout << "The first one more better!" << std::endl; 
+  std::cout << "The first one more better!" << std::endl;
 
   return 0;
 }
