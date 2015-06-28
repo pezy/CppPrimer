a155d5bebfdadf30bf01bdef9889a810386b4d0e
pezy <urbancpz@gmail.com>
2015-06-28 10:24:26 +0800

    fixed 6_25_26

--- ch06/ex6_25_26.cpp
+++ ch06/ex6_25_26.cpp
@@ -13,7 +13,10 @@
 int main(int argc, char** argv)
 {
     std::string str;
-    for (int i = 1; i != argc; ++i) str += argv[i] + " ";
+    for (int i = 1; i != argc; ++i) {
+        str += argv[i];
+        str += " ";
+    }
 
     std::cout << str << std::endl;
     return 0;
