c0f487d0cd8cfd873d70227d61f25e0e11e978da
pezy_mbp <urbancpz@gmail.com>
2015-07-27 23:52:50 +0800

    fixed #36

--- ch06/ex6_42.cpp
+++ ch06/ex6_42.cpp
@@ -1,13 +1,17 @@
-//! @Yue Wang
+//! @creator by Wang Yue
+//! @refactor by pezy
 //!
-//! Exercise 6.42:
+//! @date 27, July. 2015
+//!
+//! @question
 //! Give the second parameter of make_plural (§ 6.3.2, p. 224) a default
-//! argument of 's'. Test your program by printing singular and plural
-//! versions of the words success and failure.
+//! argument of 's'. Test your program by printing singular and plural versions
+//! of the words success and failure.
 //!
 
 #include <iostream>
 #include <string>
+
 using std::string;
 using std::cout;
 using std::endl;
@@ -19,9 +23,9 @@ string make_plural(size_t ctr, const string& word, const string& ending = "s")
 
 int main()
 {
-    cout << "singual: " << make_plural(1, "success") << " "
+    cout << "singual: " << make_plural(1, "success", "es") << " "
          << make_plural(1, "failure") << endl;
-    cout << "plural : " << make_plural(2, "success") << " "
+    cout << "plural : " << make_plural(2, "success", "es") << " "
          << make_plural(2, "failure") << endl;
 
     return 0;
