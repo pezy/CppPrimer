e6d96319215e1d06a48f4d684e279a52bc85727b
pezy <urbancpz@gmail.com>
2016-10-07 09:49:22 +0800

    fixed ex6_04 from an email issue.

--- ch06/ex6_04.cpp
+++ ch06/ex6_04.cpp
@@ -1,39 +1,36 @@
 /*
 =================================================================================
-
 C++ Primer 5th Exercise Answer Source Code
 Copyright (C) 2014-2015 github.com/pezy/CppPrimer
 
-Write a function that interacts with the user, asking for a
-number and generating the factorial of that number. Call this function from
-main.
+Write a function that interacts with the user, asking for a number and
+generating the factorial of that number. Call this function from main.
 
-If you have questions, try to connect with me: pezy<urbancpz@gmail.com>
+About the magic number(13): https://github.com/Mooophy/Cpp-Primer/pull/172
 
+If you have questions, try to connect with me: pezy<pezy.dev@gmail.com>
 =================================================================================
 */
 
 #include <iostream>
+#include <string>
 
-void factorial_with_interacts()
+int fact(int val)
 {
-    int num;
-    std::cout << "Please input a positive number: ";
-    while (std::cin >> num && num < 0)
-        std::cout << "Please input a positive number again: ";
-    std::cout << num;
+	int ret = 1;
+	while (val > 1)
+		ret *= val--;
+	return ret;
+}
 
-    unsigned long long result = 1;
-    while (num > 1) result *= num--;
-
-    std::cout << "! is ";
-    if (result)
-        std::cout << result << std::endl;
-    else
-        std::cout << "too big" << std::endl;
+void factorial_with_interacts() {
+	for (int val = 0; std::cout << "Enter a number within [0, 13): ", std::cin >> val; ) {
+		if (val < 0 || val > 12) continue;
+		std::cout << val << "! =" << fact(val) << std::endl;
+	}
 }
 
 int main()
 {
-    factorial_with_interacts();
+	factorial_with_interacts();
 }
