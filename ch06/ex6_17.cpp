50e8a37920cdb747f4fa763aa66637bca84104b3
pezy_mbp <urbancpz@gmail.com>
2016-10-11 23:14:29 +0800

    improving #82

--- ch06/ex6_17.cpp
+++ ch06/ex6_17.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <string>
 
-using std::cout;
-using std::endl;
 using std::string;
 
 bool hasUppercase(const string& str)
@@ -12,18 +10,16 @@ bool hasUppercase(const string& str)
     return false;
 }
 
-void makeLowercase(string& str)
+const string& makeLowercase(string& str)
 {
     for (auto& c : str)
         if (isupper(c)) c = tolower(c);
+    return str;
 }
 
 int main()
 {
     string str("Hello World!");
-    cout << hasUppercase(str) << endl;
-    makeLowercase(str);
-    cout << str << endl;
-
-    return 0;
+    std::cout << std::boolalpha << hasUppercase(str) << std::endl;
+    std::cout << makeLowercase(str) << std::endl;
 }
