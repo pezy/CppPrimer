e6e0436a70efc07b321cc09e5642393b9ba5cc8d
pezy_mbp <urbancpz@gmail.com>
2016-10-17 00:05:30 +0800

    improving #84 format.

--- ch09/ex9_43.cpp
+++ ch09/ex9_43.cpp
@@ -19,19 +19,18 @@ using std::string;
 
 void Replace(string& s, const string& oldVal, const string& newVal)
 {
-	for (auto beg = s.begin(); beg != s.end(); ) {
-		if (std::distance(beg, s.end()) <
-			std::distance(oldVal.begin(), oldVal.end()))
-			break;
-		if (*beg == oldVal.front() && string{ beg, beg + oldVal.size() } == oldVal) {
-			beg = s.erase(beg, beg + oldVal.size());
-			beg = s.insert(beg, newVal.cbegin(), newVal.cend()) + newVal.size();
-		}
-		else {
-			++beg;
-		}
-	}
+    for (auto beg = s.begin(); std::distance(beg, s.end()) >=
+                               std::distance(oldVal.begin(), oldVal.end());) {
+        if (string{beg, beg + oldVal.size()} == oldVal) {
+            beg = s.erase(beg, beg + oldVal.size());
+            beg = s.insert(beg, newVal.cbegin(), newVal.cend());
+            std::advance(beg, newVal.size());
+        }
+        else
+            ++beg;
+    }
 }
+
 int main()
 {
     {
