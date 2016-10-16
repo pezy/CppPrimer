000a1d3bedb5438ee2a51f63bd304490bb69eac5
sanerror <237606586@qq.com>
2016-10-16 16:36:03 +0800

    Update ex9_43
    `erase()` may affect the existed iterator `beg`;
    and improve the intelligibility.

--- ch09/ex9_43.cpp
+++ ch09/ex9_43.cpp
@@ -18,18 +18,17 @@ using std::string;
 
 void Replace(string& s, const string& oldVal, const string& newVal)
 {
-    for (auto beg = s.begin(); beg != s.end(); ++beg) {
-        if (*beg != oldVal.front()) continue;
-        if (std::distance(beg, s.end()) <
-            std::distance(oldVal.begin(), oldVal.end()))
-            break;
-        if (string{beg, beg + oldVal.size()} == oldVal) {
-            auto pos = std::distance(s.begin(), beg);
-            s.erase(beg, beg + oldVal.size());
-            s.insert(beg, newVal.cbegin(), newVal.cend());
-            beg = std::next(s.begin(), pos + newVal.size() - 1);
-        }
-    }
+	for (auto beg = s.begin(); beg != s.end(); ) {
+		++beg;
+		if (*beg != oldVal.front()) continue; 
+		if (std::distance(beg, s.end()) <
+			std::distance(oldVal.begin(), oldVal.end()))
+			break;
+		if (string{ beg, beg + oldVal.size() } == oldVal) {
+			beg = s.erase(beg, beg + oldVal.size());
+			beg = s.insert(beg, newVal.cbegin(), newVal.cend()) + newVal.size();
+		}
+	}
 }
 
 int main()
