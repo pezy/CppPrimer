49291a57e4645266197fadce45da6c13cda85892
pezy_mbp <urbancpz@gmail.com>
2017-05-15 00:34:58 +0800

    fixed #121, thx @why2cs.

--- ch11/ex11_31.cpp
+++ ch11/ex11_31.cpp
@@ -10,9 +10,9 @@
 //  Be sure your program works correctly if the element you look for is not in
 //  the map.
 
+#include <iostream>
 #include <map>
 #include <string>
-#include <iostream>
 
 using std::string;
 
@@ -20,20 +20,18 @@ int main()
 {
     std::multimap<string, string> authors{
         {"alan", "DMA"}, {"pezy", "LeetCode"}, {"alan", "CLRS"},
-        {"wang", "FTP"}, {"pezy", "CP5"},      {"wang", "CPP-Concurrency"}};
-    // want to delete an element that author is [Alan], work is [112].
+        {"wang", "FTP"}, {"pezy", "CP5"},      {"wang", "CPP-Concurrency"},
+        {"pezy", "CP5"}};
+    // want to delete an element that author is [pezy], work is [CP5].
     string author = "pezy";
     string work = "CP5";
 
-    auto found = authors.find(author);
-    auto count = authors.count(author);
-    while (count) {
-        if (found->second == work) {
-            authors.erase(found);
-            break;
-        }
-        ++found;
-        --count;
+    for (auto found = authors.find(author);
+         found != authors.end() && found->first == author;) {
+        if (found->second == work)
+            found = authors.erase(found);
+        else
+            ++found;
     }
 
     for (const auto& author : authors)
