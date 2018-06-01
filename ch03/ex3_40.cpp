eb1c9c55ddb2c4efeeb648b81f92bccd773c0611
pezy chen <mail@devnotes.org>
2018-06-01 16:35:27 +0800

    Fixed #142

--- ch03/ex3_40.cpp
+++ ch03/ex3_40.cpp
@@ -1,17 +1,26 @@
-#include <iostream>
+// Write a program to define two character arrays initialized
+// from string literals. Now define a third character array to hold the
+// concatenation of the two arrays. Use `strcpy` and `strcat` to copy the two
+// arrays into the third.
+
 #include <cstring>
+#include <iostream>
 
 using std::cout;
 using std::endl;
 
 int main()
 {
-    constexpr size_t new_size = strlen(cstr1) + strlen(" ") + strlen(cstr2) + 1;
-    char cstr3[new_size];
+    const char cstr1[] = "Hello";
+    const char cstr2[] = "world!";
+
+    size_t new_size = strlen(cstr1) + strlen(" ") + strlen(cstr2) + 1;
+    char* cstr3 = new char[new_size];
 
     strcpy(cstr3, cstr1);
     strcat(cstr3, " ");
     strcat(cstr3, cstr2);
 
     std::cout << cstr3 << std::endl;
+    delete [] cstr3;
 }
