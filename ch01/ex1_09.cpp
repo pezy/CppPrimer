aa9b6aaec55b2c36c59511bce62a230198434116
Prateekgupta9819 <36070161+Prateekgupta9819@users.noreply.github.com>
2018-10-30 13:12:32 +0530

    Update ex1_09.cpp

--- ch01/ex1_09.cpp
+++ ch01/ex1_09.cpp
@@ -1,18 +1,13 @@
-// sum the numbers from 50 to 100 (use while)
-
-#include <iostream>
-
+#include <bits/stdc++.h>
+using namespace std;
 int main()
 {
-    int sum = 0, val = 50;
+    int sum = 0, val ;
+    cout<<"Enter the value \n";
     while (val <= 100) {
         sum += val;
         ++val;
     }
-
-    std::cout << "the sum is: " << sum << std::endl;
-
+    cout<<"the sum is: "<<sum<<endl;
     return 0;
 }
-
-// output: the sum is: 3825
