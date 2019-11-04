277a9201c5e5f3117e5b31419af6d3ec5195234c
Escape0707 <tothesong@gmail.com>
2019-11-04 22:59:32 +0800

    Fix 13.8
    "copy the object to which ps points" is enough.

--- ch13/ex13_08.h
+++ ch13/ex13_08.h
@@ -25,9 +25,7 @@ public:
     HasPtr(const HasPtr& hp) : ps(new std::string(*hp.ps)), i(hp.i) {}
     HasPtr& operator=(const HasPtr& hp)
     {
-        std::string* new_ps = new std::string(*hp.ps);
-        delete ps;
-        ps = new_ps;
+        *ps = *hp.ps;
         i = hp.i;
         return *this;
     }
